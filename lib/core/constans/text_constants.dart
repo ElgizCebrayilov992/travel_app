cf0ef251ca9a5ac48755fecd7adcca71dd45d5b2
Elgiz Cebrayilov <elgizcebrayilov1992@gmail.com>
2021-08-27 21:25:45 +0400

    Travel App

---
+++ lib/core/constans/text_constants.dart
@@ -0,0 +1,7 @@
+class UITrravelConstans {
+  static final String title = 'Welcome';
+  static final String onBoardingFirst = 'Excart Lacation';
+  static final String onBoardingSecond =
+      'We travel together all around the word';
+  static final String onBoardingButton = 'Lets go';
+}
---
+++ lib/future/view/onBoardingPage/onboarding_view_page.dart
@@ -0,0 +1,85 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:kartal/kartal.dart';
+import 'package:travel_app/core/constans/app_constantsa.dart';
+import 'package:travel_app/core/constans/text_constants.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: context.paddingMedium,
+          child: Column(
+            children: [
+              buildTitle(context),
+              buildImagesText(context),
+              Container(
+                //color: Colors.red,
+                height: context.dynamicHeight(0.08),
+                width: context.dynamicWidth(0.4),
+                child: ElevatedButton(onPressed: () {
+                  
+                }, child: Text(UITrravelConstans.onBoardingButton),style: ElevatedButton.styleFrom(primary: Colors.indigo,textStyle: context.textTheme.headline5),),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+   buildImagesText(BuildContext context) {
+    return Expanded(
+      child: Column(
+        children: [
+          Container(
+            height: context.dynamicHeight(0.4),
+            width: context.dynamicWidth(100),
+            child: Image(
+              image: AssetImage(AppConstants.imageUrl),
+            ),
+          ),
+          Text(
+            UITrravelConstans.onBoardingFirst,
+            style: context.textTheme.headline4!
+                .copyWith(fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          SizedBox(
+            height: context.dynamicHeight(0.05),
+          ),
+          AnimatedTextKit(
+            animatedTexts: [
+              TypewriterAnimatedText(UITrravelConstans.onBoardingSecond,
+                  textStyle: context.textTheme.subtitle1!.copyWith(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.amber,
+                  ),
+                  textAlign: TextAlign.center),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Container buildTitle(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      height: context.dynamicHeight(0.08),
+      child: AnimatedTextKit(
+        //displayFullTextOnTap: true,
+        animatedTexts: [
+          WavyAnimatedText(
+            UITrravelConstans.title,
+            textStyle: context.textTheme.headline3!
+                .copyWith(fontWeight: FontWeight.bold, color: Colors.indigo),
+          ),
+        ],
+        isRepeatingAnimation: true,
+      ),
+    );
+  }
+}
